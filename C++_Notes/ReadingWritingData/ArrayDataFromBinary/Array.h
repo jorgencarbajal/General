9fb9f4d6753c6c93bcea09e6ed01524e99d2bdf3
jorgencarbajal <jorgecarbajal39@yahoo.com>
2025-09-09 11:02:23 -0700

    Create Array.h

---
+++ C++_Notes/ReadingWritingData/ArrayDataFromBinary/Array.h
@@ -0,0 +1,13 @@
+/* 
+ * Purpose:  Specification for the Array structure
+ */
+
+ #ifndef ARRAY_H
+ #define ARRAY_H
+ 
+ struct Array{
+     int size;  //Size of the array
+     int *data; //Data in the array
+ };
+ 
+ #endif /* ARRAY_H */
