e5fb2460deed669fb1f27fe5fe7294aefc5f4a4a
jorgencarbajal <jorgecarbajal39@yahoo.com>
2025-09-09 10:59:35 -0700

    Create MovieInfo.h

---
+++ C++_Notes/MoveDataStructureStepByStep/11_MultipleFiles/MovieInfo.h
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ */
+
+ #ifndef MOVIEINFO_H
+ #define MOVIEINFO_H
+ 
+ struct MovieInfo{
+     string movName;//Strings are good
+     char *dirName;//You should know how to deal with dynamic character arrays
+     unsigned short yrRlsd; //Don't need a larger data size and always > 0
+     unsigned short runTime;//Don't need a larger data size and always > 0
+ };
+ 
+ #endif /* MOVIEINFO_H */
+ 
