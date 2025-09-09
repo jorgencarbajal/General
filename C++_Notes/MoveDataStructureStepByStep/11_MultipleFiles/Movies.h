3f95fcc27577fb74c0bc8276734d07012a0d9c40
jorgencarbajal <jorgecarbajal39@yahoo.com>
2025-09-09 11:00:05 -0700

    Create Movies.h

---
+++ C++_Notes/MoveDataStructureStepByStep/11_MultipleFiles/Movies.h
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ */
+
+ #ifndef MOVIES_H
+ #define MOVIES_H
+ 
+ #include "MovieInfo.h"
+ 
+ struct Movies{
+     int nMovies;
+     MovieInfo *mveInfo;
+ };
+ 
+ #endif /* MOVIES_H */
+ 
